45db65c5cae1277566531b778ad067e1015e0674
Sujata Sahu <120352925+sahu00001@users.noreply.github.com>
2023-06-20 13:07:16 -0500

    Update sujata.py

--- Python/RoboDK/sujata.py
+++ Python/RoboDK/sujata.py
@@ -1,1 +1,48 @@
+import os
+import sys
+import time
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
+
+# Receive variables from RoboDK
+move = RDK.getParam('move')  # Make sure the 'move' parameter is set correctly in RoboDK
+dir = RDK.getParam('dir')    # Make sure the 'dir' parameter is set correctly in RoboDK
+speed = RDK.getParam('speed')  # Make sure the 'speed' parameter is set correctly in RoboDK
+
+from xarm.wrapper import XArmAPI
+from configparser import ConfigParser
+
+parser = ConfigParser()
+parser.read('../robot.conf')
+
+try:
+    ip = parser.get('xArm', 'ip')
+except:
+    print("Failed to read IP from robot.conf")
+    sys.exit(0)
+
+arm = XArmAPI(ip)
+time.sleep(0.5)
+
+if arm.warn_code != 0:
+    arm.clean_warn()
+if arm.error_code != 0:
+    arm.clean_error()
+
+arm.motion_enable(True)
+arm.set_mode(0)
+arm.set_state(0)
+
+code = arm.set_tgpio_modbus_timeout(20)
+print('set_tgpio_modbus_timeout, code={}'.format(code))
+
+# Set tool gpio modbus baudrate
+code = arm.set_tgpio_modbus_baudrate(115200)
+print('set_tgpio_modbus_baudrate, code={}'.format(code))
+
+time.sleep(1)
+
+data_frame = [0x08, 0x10, 0x00, 0x01, 0x00, 0x03, 0x06, 0x00, move, 0x00, dir, 0x00, speed]
+ret = arm.core.tgpio_set_modbus(data_frame, len(data_frame))  # Setup these variables as hex values
+
+time.sleep(2)
 
